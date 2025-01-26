f4c4b3993510bdb259a6a7db2230b009801778a7
Krishanu Mandal <krishanu178@gmail.com>
2025-01-26 17:40:12 +0530

    get params using useSearchParams hooks and redirect properly with this video in /watch

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { closeMenu } from '../../utils/appSlice';
+import { useSearchParams } from 'react-router-dom';
 
 const WatchPage = () => {
+  const [searchParams] = useSearchParams(); //get url params (browser search params)
+  console.log(searchParams.get("v"));
 
   const dispatch = useDispatch();
 
@@ -12,7 +15,10 @@ const WatchPage = () => {
 
 
   return (
-    <div>WatchPage</div>
+    <div className='px-5 '>
+      <iframe width="1200" height="600" src={"https://www.youtube.com/embed/"+searchParams.get("v")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullscreen></iframe>
+
+    </div>
   )
 }
 
